4a00362d318cf9ed056042400946e2494d6e43cc
German <metzgergerman@gmail.com>
2025-06-29 20:21:52 -0300

    arreglo de bugs celular

--- src/Components/InfoVentana/InfoVentana.jsx
+++ src/Components/InfoVentana/InfoVentana.jsx
@@ -14,13 +14,13 @@ export default function InfoVentana({cerrarVentana}) {
                 <h2>Game Information</h2>
                 <h3>Commands</h3>
                 <div>
-                    <p><strong>M </strong> = Mute/unmute background music</p>
-                    <p><strong>C </strong> = Change background music</p>
-                    <p><strong>- </strong> = Decrease volume</p>
-                    <p><strong>+ </strong> = Increase volume</p>
-                    <p><strong>I </strong> = Show/hide game information</p>
-                    <p><strong>U </strong> = View Ladder</p>
-                    <p><strong>Esc </strong> = Go back to main menu</p>
+                    <p><strong style={{color: "yellow"}}>M </strong> = Mute/unmute background music</p>
+                    <p><strong style={{color: "yellow"}}>C </strong> = Change background music</p>
+                    <p><strong style={{color: "yellow"}}>- </strong> = Decrease volume</p>
+                    <p><strong style={{color: "yellow"}}>+ </strong> = Increase volume</p>
+                    <p><strong style={{color: "yellow"}}>I </strong> = Show/hide game information</p>
+                    <p><strong style={{color: "yellow"}}>U </strong> = View Ladder</p>
+                    <p><strong style={{color: "yellow"}}>Esc </strong> = Go back to main menu</p>
                 </div>
 
                 <div>
@@ -30,7 +30,7 @@ export default function InfoVentana({cerrarVentana}) {
                     <p><img src={publico} alt="audience" style={{backgroundColor: 'white', padding: '5px', borderRadius: '5px'}}/> = Ask the audience for their opinion</p>
                     <p><img src={cambio} alt="switch" style={{backgroundColor: 'white', padding: '5px', borderRadius: '5px'}}/> = Switch the current question</p>
                 </div>
-                <Button estadoActual='button-inactivo' children="Exit" onClick={cerrarVentana}></Button>
+                <Button estadoActual='button-inactivo' children="Exit" style={{width: "50%"}} onClick={cerrarVentana}></Button>
 
             </div>
         </div>
